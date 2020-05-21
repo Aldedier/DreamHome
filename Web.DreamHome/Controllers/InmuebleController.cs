97f44e9ae739e53c2911a30bd02bb90d21d37007
ALDEDIER MARTINEZ <aldedier@live.com>
2020-05-21 00:38:37 -0500

    Correción en editar

--- Web.DreamHome/Controllers/InmuebleController.cs
+++ Web.DreamHome/Controllers/InmuebleController.cs
@@ -16,40 +16,31 @@
 
         public ActionResult Crear()
         {
-            ViewBag.IDF_TIPO_INM = new SelectList(new ListasRepositorio().ConsultarTiposPropiedades(), "Value", "Text");
+            ViewBag.IDF_TIPO_INM = new SelectList(new ListasRepositorio().ConsultarTiposPropiedades(), "ID_TIPO", "NOMBRE_TIPO");
             ViewBag.Mensaje = null;
-
             return View();
         }
 
         [HttpPost]
         public ActionResult Crear(InmueblesDTO inmueblesDTO)
         {
-            ViewBag.IDF_TIPO_INM = new SelectList(new ListasRepositorio().ConsultarTiposPropiedades(), "Value", "Text");
-
             inmueblesDTO.SESSION = GetSession();
-
+            ViewBag.IDF_TIPO_INM = new SelectList(new ListasRepositorio().ConsultarTiposPropiedades(), "ID_TIPO", "NOMBRE_TIPO", inmueblesDTO.IDF_TIPO_INM);
             return RedirectToAction("Inicial", "Inmueble", new { _mensaje = new InmueblesRepositorio().ValidarInmueble(inmueblesDTO) });
         }
 
         public ActionResult Editar(int _id)
         {
-            ViewBag.IDF_TIPO_INM = new SelectList(new ListasRepositorio().ConsultarTiposPropiedades(), "Value", "Text");
-
             InmueblesDTO datos = new InmueblesRepositorio().ConsultaInmuebles((int)GetSession()).Where(x => x.ID_INMUEBLE == _id).FirstOrDefault();
-
+            ViewBag.IDF_TIPO_INM = new SelectList(new ListasRepositorio().ConsultarTiposPropiedades(), "ID_TIPO", "NOMBRE_TIPO", datos.IDF_TIPO_INM);
             return View(datos);
         }
 
         [HttpPost]
         public ActionResult Editar(InmueblesDTO inmueblesDTO)
         {
-            ViewBag.IDF_TIPO_INM = new SelectList(new ListasRepositorio().ConsultarTiposPropiedades(), "Value", "Text");
-
+            ViewBag.IDF_TIPO_INM = new SelectList(new ListasRepositorio().ConsultarTiposPropiedades(), "ID_TIPO", "NOMBRE_TIPO", inmueblesDTO.IDF_TIPO_INM);
             inmueblesDTO.SESSION = GetSession();
-
-            InmueblesDTO datos = new InmueblesRepositorio().ConsultaInmuebles((int)inmueblesDTO.SESSION).Where(x => x.ID_INMUEBLE == inmueblesDTO.ID_INMUEBLE).FirstOrDefault();
-
             return RedirectToAction("Inicial", "Inmueble", new { _mensaje = new InmueblesRepositorio().ActualizarInmueble(inmueblesDTO) });
         }
 
